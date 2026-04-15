9ef835940b51383360a3af6bcd0070be04db9e17
joelkaudzu9-ship-it <joelkaudzu9@gmail.com>
2026-04-15 23:00:56 +0200

    Remove Flask-Mail import since using Keplars now

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 from certificate import generate_certificate, check_subject_completion
 from models import db, User, Subject, Lesson, Payment, EmailVerification, PasswordReset, Progress, LoginAttempt
 from forms import LoginForm, RegistrationForm, PaymentForm, RequestResetForm, ResetPasswordForm
-from email_utils import mail, send_verification_email, send_welcome_email, send_payment_confirmation_email, test_smtp_connection, send_password_reset_code, send_verification_code
+from email_utils import send_verification_email, send_welcome_email, send_payment_confirmation_email, test_smtp_connection, send_password_reset_code, send_verification_code
 from paychangu import PayChangu
 
 def generate_reset_code():
